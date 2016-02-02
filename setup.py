e3a77948f2e0b68776a289e3ac75caf1b5024d7a
Nathan Cahill <nathan@nathancahill.com>
2016-02-02 16:16:46 -0700

    move docs to github

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
 """
 WKB-Raster
+----------
 
 Read WKB rasters to Numpy arrays.
-
-.. code:: python
-
-    from wkb_raster import read_wkb_raster
-
-    raster = read_wkb_raster(binary)
-    raster['bands'][0]
-
-Usage with PostGIS Rasters. Use `ST_AsBinary
-<http://postgis.net/docs/manual-dev/RT_ST_AsBinary.html>`_
-to return the WKB representation of the raster.
-
-.. code:: sql
-    SELECT ST_AsBinary(rast) FROM rasters;
-
-Wrap the returned buffer in `cStringIO.StringIO` to
-pass it to `read_wkb_raster`.
 
 Links
 `````
 
+* `Docs <https://github.com/nathancahill/wkb-raster>`_
 * `Raster WKB RFC
   <http://trac.osgeo.org/postgis/browser/trunk/raster/doc/RFC2-WellKnownBinaryFormat>`_
 * `GitHub <https://github.com/nathancahill/wkb-raster>`_
@@ -32,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='WKB-Raster',
-    version='0.2',
+    version='0.3',
     url='https://github.com/nathancahill/wkb-raster',
     license='MIT',
     author='Nathan Cahill',
