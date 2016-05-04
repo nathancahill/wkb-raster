a8368da80661fc0edc08ec8ff8ab29736a136c2d
Nathan Cahill <nathan@nathancahill.com>
2016-05-04 14:12:43 -0600

    version bump 0.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='WKB-Raster',
-    version='0.5',
+    version='0.6',
     url='https://github.com/nathancahill/wkb-raster',
     license='MIT',
     author='Nathan Cahill',
